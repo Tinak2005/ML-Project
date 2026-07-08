bb59cac3e0b634392d8d21ce78835f2a445e1668
Tina Koyal <tinakoyal225@gmail.com>
2026-07-08 18:37:10 +0530

    Fixed bugs

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            writing_score=float(request.form.get('writing_score')),
+            reading_score=float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -37,10 +37,10 @@ def predict_datapoint():
         predict_pipeline=PredictPipeline()
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        print("After Prediction")
-        print("Final Prediction:", results)
+        print("After Prediction:", results[0])
+    
         # return render_template('home.html',results=results[0])
-        return {"prediction": results[0]}
+        return {"prediction": round(results[0],2)}
     
     
 
